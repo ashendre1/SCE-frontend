f88e7953bf34593a4afbdb2a1c7995ef1fe93fe8
aniketshendre23 <aniketshendre99@gmail.com>
2025-05-07 14:09:32 -0400

    uri change

--- src/ApiService/apiService.js
+++ src/ApiService/apiService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'http://cci-ci-test.charlotte.edu:8000';
 
 export const signup = async (email, password) => {
     const response = await fetch(`${API_BASE_URL}/signup`, {
